fc1395d27f89bf9211d48e2b14165e79dd4575c4
Cristiano Salvati <crstn.salvati@gmail.com>
2022-07-09 20:49:20 +0200

    Initial Commit
    Initial Commit
    Spring project setup

---
+++ src/main/java/com/amaris/test/AmarisHomeTestApplication.java
@@ -0,0 +1,13 @@
+package com.amaris.test;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AmarisHomeTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AmarisHomeTestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/amaris/test/AmarisHomeTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.amaris.test;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AmarisHomeTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
